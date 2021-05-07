f11b3c870a6ca0a66218a911710d8d587d68d64d
Ugochukwu <maluugochukwu@gmail.com>
2021-05-07 16:31:16 +0100

    =worked on the home page

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <meta name="description" content="Medcity - Medical Healthcare HTML5 Template">
-  <link href="assets/images/logo/logo.png" rel="icon">
+  <link href="assets/images/favicon/favicon-150x150.png" rel="icon">
   <title>APIN</title>
 
   <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&amp;family=Roboto:wght@400;700&amp;display=swap">
-  <link rel="stylesheet" href="../../../use.fontawesome.com/releases/v5.15.3/css/all.css">
+    href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap">
+  <link rel="stylesheet" href="http://use.fontawesome.com/releases/v5.15.3/css/all.css">
   <link rel="stylesheet" href="assets/css/libraries.css">
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -35,7 +35,7 @@
               <div class="d-flex align-items-center justify-content-between">
                 <ul class="contact__list d-flex flex-wrap align-items-center list-unstyled mb-0">
                   <li>
-                    <button class="miniPopup-emergency-trigger" type="button">24/7 Emergency</button>
+                    <button class="miniPopup-emergency-trigger" type="button">24/7 Support</button>
                     <div id="miniPopup-emergency" class="miniPopup miniPopup-emergency text-center">
                       <div class="emergency__icon">
                         <i class="icon-call3"></i>
@@ -46,15 +46,15 @@
                       <p>Please feel free to contact our friendly reception staff with any general or medical enquiry.
                       </p>
                       <a href="appointment.html" class="btn btn__secondary btn__link btn__block">
-                        <span>Make Appointment</span> <i class="icon-arrow-right"></i>
+                        <span>Contact Us</span> <i class="icon-arrow-right"></i>
                       </a>
                     </div><!-- /.miniPopup-emergency -->
                   </li>
                   <li>
-                    <i class="icon-phone"></i><a href="tel:+5565454117">Emergency Line: (002) 01061245741</a>
+                    <i class="icon-phone"></i><a href="tel:+5565454117">Hot Line: (002) 01061245741</a>
                   </li>
                   <li>
-                    <i class="icon-location"></i><a href="#">Location: Brooklyn, New York</a>
+                    <i class="icon-location"></i><a href="#">Location: AdoEkiti, Oyo State</a>
                   </li>
                   <li>
                     <i class="icon-clock"></i><a href="contact-us.html">Mon - Fri: 8:00 am - 7:00 pm</a>
@@ -79,33 +79,17 @@
       <nav class="navbar navbar-expand-lg sticky-navbar">
         <div class="container-fluid">
           <a class="navbar-brand" href="index-2.html">
-            <img src="assets/images/logo/logo-light.png" class="logo-light" alt="logo">
-            <img src="assets/images/logo/logo-dark.png" class="logo-dark" alt="logo">
+            <img src="assets/images/logo/logo.png" class="logo-light" alt="logo">
+            <img src="assets/images/logo/logo.png" class="logo-dark" style="max-width:60%" alt="logo">
           </a>
           <button class="navbar-toggler" type="button">
             <span class="menu-lines"><span></span></span>
           </button>
           <div class="collapse navbar-collapse" id="mainNavigation">
             <ul class="navbar-nav ml-auto">
-              <li class="nav__item has-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link active">Home</a>
-                <ul class="dropdown-menu">
-                  <li class="nav__item">
-                    <a href="index-2.html" class="nav__item-link">Home Main</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="home-modern.html" class="nav__item-link">Home Modern</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="home-classic.html" class="nav__item-link">Home Classic</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="home-dentist.html" class="nav__item-link">Home Dentist</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="home-pharmacy.html" class="nav__item-link">Home pharmacy</a>
-                  </li><!-- /.nav-item -->
-                </ul><!-- /.dropdown-menu -->
+              <li class="nav__item">
+                <a href="#" data-toggle="dropdown" class="nav__item-link active">Home</a>
+                
               </li><!-- /.nav-item -->
               <li class="nav__item has-dropdown">
                 <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">About Us</a>
@@ -114,107 +98,64 @@
                     <a href="about-us.html" class="nav__item-link">About Us</a>
                   </li><!-- /.nav-item -->
                   <li class="nav__item">
-                    <a href="services.html" class="nav__item-link">Our Services</a>
+                    <a href="services.html" class="nav__item-link">Board of Trustee/Director</a>
                   </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="services-single.html" class="nav__item-link">single Services</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="pricing.html" class="nav__item-link">Pricing & Plans</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="appointment.html" class="nav__item-link">Appointments</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="faqs.html" class="nav__item-link">Help & FAQs</a>
-                  </li> <!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="gallery.html" class="nav__item-link">Our Gallery</a>
-                  </li><!-- /.nav-item -->
-                </ul><!-- /.dropdown-menu -->
-              </li><!-- /.nav-item -->
-              <li class="nav__item has-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Doctors</a>
-                <ul class="dropdown-menu">
-                  <li class="nav__item">
-                    <a href="doctors-timetable.html" class="nav__item-link">Doctors Timetable</a>
-                  </li> <!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="doctors-standard.html" class="nav__item-link">Our Doctors Standard</a>
-                  </li> <!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="doctors-modern.html" class="nav__item-link">Our Doctors Modern</a>
-                  </li> <!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="doctors-grid.html" class="nav__item-link">Our Doctors Grid</a>
-                  </li> <!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="doctors-single-doctor1.html" class="nav__item-link">Single Doctor 01</a>
-                  </li> <!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="doctors-single-doctor2.html" class="nav__item-link">Single Doctor 02</a>
-                  </li> <!-- /.nav-item -->
-                </ul><!-- /.dropdown-menu -->
-              </li><!-- /.nav-item -->
-              <li class="nav__item has-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Blog</a>
-                <ul class="dropdown-menu">
-                  <li class="nav__item">
-                    <a href="blog.html" class="nav__item-link">Blog Grid</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="blog-single-post.html" class="nav__item-link">Single Blog Post</a>
-                  </li><!-- /.nav-item -->
-                </ul><!-- /.dropdown-menu -->
-              </li><!-- /.nav-item -->
-              <li class="nav__item has-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Shop</a>
-                <ul class="dropdown-menu">
-                  <li class="nav__item">
-                    <a href="shop.html" class="nav__item-link">Our Products</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="shop-single-product.html" class="nav__item-link">Products Single</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav__item">
-                    <a href="cart.html" class="nav__item-link">Cart</a>
-                  </li><!-- /.nav-item -->
+                  
                 </ul><!-- /.dropdown-menu -->
               </li><!-- /.nav-item -->
               <li class="nav__item">
-                <a href="contact-us.html" class="nav__item-link">Contacts</a>
+                <a href="#"  class="nav__item-link">Practice</a>
+                
               </li><!-- /.nav-item -->
+              <li class="nav__item has-dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Expertise</a>
+                <ul class="dropdown-menu">
+                  <li class="nav__item">
+                    <a href="blog.html" class="nav__item-link">IRB</a>
+                  </li><!-- /.nav-item -->
+                  
+                </ul><!-- /.dropdown-menu -->
+              </li><!-- /.nav-item -->
+              <li class="nav__item has-dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Career</a>
+                <ul class="dropdown-menu">
+                  <li class="nav__item">
+                    <a href="shop.html" class="nav__item-link">Working with APIN</a>
+                  </li><!-- /.nav-item -->
+                  <li class="nav__item">
+                    <a href="shop-single-product.html" class="nav__item-link">Vacancies</a>
+                  </li><!-- /.nav-item -->
+                  <li class="nav__item">
+                    <a href="cart.html" class="nav__item-link">Volunteer</a>
+                  </li><!-- /.nav-item -->
+                </ul><!-- /.dropdown-menu -->
+              </li><!-- /.nav-item -->
+              <li class="nav__item has-dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Media Center</a>
+                <ul class="dropdown-menu">
+                  <li class="nav__item">
+                    <a href="shop.html" class="nav__item-link">News</a>
+                  </li><!-- /.nav-item -->
+                  <li class="nav__item">
+                    <a href="shop-single-product.html" class="nav__item-link">Resources</a>
+                  </li><!-- /.nav-item -->
+                  <li class="nav__item">
+                    <a href="cart.html" class="nav__item-link">Gallery</a>
+                  </li><!-- /.nav-item -->
+                  <li class="nav__item">
+                    <a href="cart.html" class="nav__item-link">Events</a>
+                  </li><!-- /.nav-item -->
+                </ul><!-- /.dropdown-menu -->
+              </li><!-- /.nav-item -->
+              
             </ul><!-- /.navbar-nav -->
             <button class="close-mobile-menu d-block d-lg-none"><i class="fas fa-times"></i></button>
           </div><!-- /.navbar-collapse -->
           <div class="d-none d-xl-flex align-items-center position-relative ml-30">
-            <div class="miniPopup-departments-trigger">
-              <span class="menu-lines" id="miniPopup-departments-trigger-icon"><span></span></span>
-              <a href="departments.html">Departments</a>
-            </div>
-            <ul id="miniPopup-departments" class="miniPopup miniPopup-departments dropdown-menu">
-              <li class="nav__item">
-                <a href="department-single.html" class="nav__item-link">Neurology Clinic</a>
-              </li><!-- /.nav-item -->
-              <li class="nav__item">
-                <a href="department-single.html" class="nav__item-link">Cardiology Clinic</a>
-              </li><!-- /.nav-item -->
-              <li class="nav__item">
-                <a href="department-single.html" class="nav__item-link">Pathology Clinic</a>
-              </li><!-- /.nav-item -->
-              <li class="nav__item">
-                <a href="department-single.html" class="nav__item-link">Laboratory Clinic</a>
-              </li><!-- /.nav-item -->
-              <li class="nav__item">
-                <a href="department-single.html" class="nav__item-link">Pediatric Clinic</a>
-              </li><!-- /.nav-item -->
-              <li class="nav__item">
-                <a href="department-single.html" class="nav__item-link">Cardiac Clinic</a>
-              </li><!-- /.nav-item -->
-            </ul> <!-- /.miniPopup-departments -->
+            
             <a href="appointment.html" class="btn btn__primary btn__rounded ml-30">
               <i class="icon-calendar"></i>
-              <span>Appointment</span>
+              <span>Contct Us</span>
             </a>
           </div>
         </div><!-- /.container -->
@@ -228,13 +169,13 @@
       <div class="slick-carousel m-slides-0"
         data-slick='{"slidesToShow": 1, "arrows": true, "dots": false, "speed": 700,"fade": true,"cssEase": "linear"}'>
         <div class="slide-item align-v-h">
-          <div class="bg-img"><img src="assets/images/sliders/1.jpg" alt="slide img"></div>
+          <div class="bg-img"><img src="assets/images/sliders/hepatitis.jpg" alt="slide img"></div>
           <div class="container">
             <div class="row align-items-center">
               <div class="col-sm-12 col-md-12 col-lg-12 col-xl-7">
                 <div class="slide__content">
-                  <h2 class="slide__title">Providing Best Medical Care</h2>
-                  <p class="slide__desc">The health and well-being of our patients and their health care team will
+                  <h2 class="slide__title" style="color:#b3d55e">APIN IS IN THE NEWS</h2>
+                  <p class="slide__desc" style="color:#ffffff">The health and well-being of our patients and their health care team will
                     always be our priority, so we follow the best practices for cleanliness.</p>
                   <ul class="features-list list-unstyled mb-0 d-flex flex-wrap">
                     <!-- feature item #1 -->
@@ -242,28 +183,28 @@
                       <div class="feature__icon">
                         <i class="icon-heart"></i>
                       </div>
-                      <h2 class="feature__title">Examination</h2>
+                      <h2 class="feature__title" style="color:#ffffff" >Examination</h2>
                     </li><!-- /.feature-item-->
                     <!-- feature item #2 -->
                     <li class="feature-item">
                       <div class="feature__icon">
                         <i class="icon-medicine"></i>
                       </div>
-                      <h2 class="feature__title">Prescription </h2>
+                      <h2 class="feature__title" style="color:#ffffff">Prescription </h2>
                     </li><!-- /.feature-item-->
                     <!-- feature item #3 -->
                     <li class="feature-item">
                       <div class="feature__icon">
                         <i class="icon-heart2"></i>
                       </div>
-                      <h2 class="feature__title">Cardiogram</h2>
+                      <h2 class="feature__title" style="color:#ffffff">Cardiogram</h2>
                     </li><!-- /.feature-item-->
                     <!-- feature item #4 -->
                     <li class="feature-item">
                       <div class="feature__icon">
                         <i class="icon-blood-test"></i>
                       </div>
-                      <h2 class="feature__title">Blood Pressure</h2>
+                      <h2 class="feature__title"  style="color:#ffffff">Blood Pressure</h2>
                     </li><!-- /.feature-item-->
                   </ul><!-- /.features-list -->
                 </div><!-- /.slide-content -->
@@ -286,7 +227,7 @@
                       <div class="feature__icon">
                         <i class="icon-heart"></i>
                       </div>
-                      <h2 class="feature__title">Examination</h2>
+                      <h2 class="feature__title" style="color:#fff">Examination</h2>
                     </li><!-- /.feature-item-->
                     <!-- feature item #2 -->
                     <li class="feature-item">
